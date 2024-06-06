1a5460e9367b7093cf16dee6fb49738b5dc8dcdb
Harlynton <harlynton.castano@gmail.com>
2024-06-06 13:03:46 -0500

    Configuración de firestore y creación de funcionalidad de carga simple de archivos zip.

--- src/components/FileManager.vue
+++ src/components/FileManager.vue
@@ -2,14 +2,48 @@
 <template>
   <div>
     <h2>File Manager</h2>
-    <!-- Contenido del FileManager -->
+    <input type="file" @change="onFileChange" accept=".zip" />
+    <button @click="uploadFile" :disabled="!file">Upload</button>
+    <p v-if="message">{{ message }}</p>
   </div>
 </template>
 
 <script>
+import { storage, ref, uploadBytes } from '../firebase';
+
 export default {
-  name: 'FileManager'
-}
+  name: 'FileManager',
+  data() {
+    return {
+      file: null,
+      message: ''
+    };
+  },
+  methods: {
+    onFileChange(event) {
+      const selectedFile = event.target.files[0];
+      console.log(selectedFile.type)
+      if (selectedFile && selectedFile.type === 'application/x-zip-compressed') {
+        this.file = selectedFile;
+        this.message = '';
+      } else {
+        this.message = 'Please select a ZIP file';
+        this.file = null;
+      }
+    },
+    async uploadFile() {
+      if (this.file) {
+        const storageRef = ref(storage, this.file.name);
+        try {
+          await uploadBytes(storageRef, this.file);
+          this.message = 'File uploaded successfully!';
+        } catch (error) {
+          this.message = `Error uploading file: ${error.message}`;
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDj21GoVwPAcRganDdD_QTOKrM0_1ND4bg",
+  authDomain: "vue2-demo-3b507.firebaseapp.com",
+  projectId: "vue2-demo-3b507",
+  storageBucket: "vue2-demo-3b507.appspot.com",
+  messagingSenderId: "404539118758",
+  appId: "1:404539118758:web:25f080fdc86da877d8d067",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export { storage, ref, uploadBytes };
